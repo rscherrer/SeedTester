f14f8b3dca2238d64c349f4a62d2e73a74f8ea59
rscherrer <raphael.scherrer@evobio.eu>
2019-07-23 15:59:31 +0200

    seed reset works locally, what do you say travis?

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <random>
 
+
+/// Function to seed RNG and sample a Poisson number
+size_t seedAndSample(const size_t &seed, const double &lambda, std::mt19937_64 &rng)
+{
+    rng.seed(seed);
+    return std::poisson_distribution<int>(lambda)(rng);
+}
+
+
+/// Main function
 int main()
 {
 
     std::mt19937_64 rng;
 
-    rng.seed(42u);
+    std::cout << "Randomly generated numbers = ";
 
-    std::cout << "Hello world!\n";
+    for (size_t i = 0u; i < 5u; ++i)
+    {
+        std::cout << seedAndSample(42u, 2.0, rng) << ' ';
+        //std::cout << seedAndSample(i, 2.0, rng) << ' ';
+    }
+
+    std::cout << '\n';
+
     return 0;
 }

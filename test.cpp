c8b50cf922886c40be9eded2ca47172356ee2bdc
rscherrer <raphael.scherrer@evobio.eu>
2019-07-24 13:29:48 +0200

    go travis

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 
 #include <boost/test/included/unit_test.hpp>
 #include "functions.h"
-#include <random>
+#include "random.h"
 #include <iostream>
 
 BOOST_AUTO_TEST_CASE(dummyTest)
@@ -12,9 +12,8 @@ BOOST_AUTO_TEST_CASE(dummyTest)
 
 BOOST_AUTO_TEST_CASE(checkRNG)
 {
-    std::mt19937_64 rng;
-    rng.seed(42u);
-    std::vector<size_t> numbers = samplePoissonVector(5u, 2.0, rng);
+    rnd::rng.seed(42u);
+    std::vector<size_t> numbers = samplePoissonVector(5u, 2.0, rnd::rng);
     std::vector<size_t> expected { 3u, 1u, 2u, 1u, 5u };
     BOOST_CHECK_EQUAL_COLLECTIONS(numbers.begin(), numbers.end(), expected.begin(), expected.end());
 }
